ab25b9af5606c2b1c4ba6f126b239111451cc4b9
RatulGhosh <ratulghoshr@gmail.com>
2015-10-22 05:09:32 +0530

    Adding users

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require('express'),
+ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	usernames = []; // Array of Connected Users
 	
 server.listen(process.env.PORT || 3000);
 
@@ -10,11 +11,31 @@ app.get('/', function(req, res){
 });
 
 io.sockets.on('connection', function(socket){
+	socket.on('new user', function(data, callback){
+		if((usernames.indexOf(data) != -1)){
+			callback(false);
+		} else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	// Update Usernames
+	function updateUsernames(){
+		io.sockets.emit('usernames', usernames);
+	}
 
 	// Send Message
 	socket.on('send message', function(data){
-		io.sockets.emit('new message',{msg: data});
+		io.sockets.emit('new message',{msg: data, user: socket.username});
 	});
 
-	
+	// Disconnect
+	socket.on('disconnect', function(data){
+		if(!socket.username) return;
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
+	});
 });
